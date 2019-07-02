0f50aaeebf8e856131197f1cf0cc0cec0e181975
Taylor Brockman <taylor.brockman@gmail.com>
2019-07-02 15:29:10 -0400

    Example pobvray generator

--- python/piper.py
+++ python/piper.py
@@ -2,6 +2,14 @@
 
 import fileinput
 
+zoomLevel = -300
+
+## print lightsource and camera
+print("#include \"colors.inc\"")
+print("camera {\nlocation <0, 0, %d>\nlook_at  <0, 0,0> }\n" % zoomLevel )
+print("light_source { <2, 4, -3> color White}")
+print("background { color Cyan }")
+
 for line in fileinput.input():
     # Parse
     arr = line.rstrip().split(",")
@@ -14,6 +22,8 @@ for line in fileinput.input():
         index = int(arr[0])
         fibb = int(arr[1])
         fibb2 = fibb * 2
-        print("%d,%d,%d" % ( index, fibb, fibb2 ))
+        rad = index * 0.1
+        print("// %d,%d,%d" % ( index, fibb, fibb2 ))
+        print("sphere{ <%d,%d,0>, %f color Blue }" % ( fibb, fibb, rad ) )
     pass
 
